8f32bf20fff2513fe693a5646b2aafbee5ba732f
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-09-24 00:48:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,23 @@
 class Waiter
+  
+  attr_accessor :name, :yrs_experience
+  
+  @@all = []
+  
+  def initialize(name, yrs_experience)
+    @name = name
+    @Yrs_experience = yrs_experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def meals
+    Meals.all.select do |meal|
+      meal.water == self
+    end
+  end
 
 end
